c7d0e5d21a48ea6d47f1558c08e6227bc063f04b
Rahul Ekbote <rahulek@gmail.com>
2023-01-21 18:37:43 +0530

    Polymorphism code added

--- Source/Interface/interfaceComposition.go
+++ Source/Interface/interfaceComposition.go
@@ -35,6 +35,12 @@ type Dog struct {
 	MinSleepHours int
 }
 
+// Another Concrete type that implements the Animal interface
+type Human struct {
+	Name string
+}
+
+// ---- Dog implementing the Animal behavior ----------
 func (d *Dog) Walk() string {
 	return fmt.Sprintf("\tDog %s is walking on all 4 with its %s tail\n", d.Name, d.Tail)
 }
@@ -54,6 +60,19 @@ func (d *Dog) Sleep(numHours int) string {
 	return ret
 }
 
+// ---- Human implementing the Animal behavior ----------
+func (h *Human) Walk() string {
+	return fmt.Sprintf("\tHuman %s is walking on his 2.\n", h.Name)
+}
+
+func (h *Human) Eat(food string) string {
+	return fmt.Sprintf("\tHuman %s is eating %s happily.\n", h.Name, food)
+}
+
+func (h *Human) Sleep(numHours int) string {
+	return fmt.Sprintf("\tHuman %s really Happy, its going to sleep now.\n", h.Name)
+}
+
 // InterfaceCompositionDemo function accepts title parameter
 // and demonstrates the interface composition technique
 func InterfaceCompositionDemo(title string) {
@@ -69,6 +88,14 @@ func InterfaceCompositionDemo(title string) {
 	fmt.Printf("\n")
 }
 
+func animalOperations(a Animal) {
+	fmt.Printf("\tanimalOperations: Start\n")
+	fmt.Printf("\t\t%s", a.Walk())
+	fmt.Printf("\t\t%s", a.Eat("Meat and Eggs"))
+	fmt.Printf("\t\t%s", a.Sleep(12))
+	fmt.Printf("\tanimalOperations: End\n")
+}
+
 // TypeConversionDemo function accepts title parameter
 // and demonstrates the type conversion of interfaces
 func TypeConversionDemo(title string) {
@@ -80,6 +107,13 @@ func TypeConversionDemo(title string) {
 	var w Walker = Walker(a)
 	e, s := Eater(a), Sleeper(a)
 	fmt.Printf("\td: %T, a: %T, w: %T, e: %T, s: %T\n", d, a, w, e, s)
+
+	//'a' is an Animal with an underlying type of a Dog
+	animalOperations(a)
+
+	var h Human = Human{Name: "James"}
+	//Now h is also an animal
+	animalOperations(Animal(&h))
 
 	fmt.Printf("*** %v : END ***\n", title)
 	fmt.Printf("\n")
@@ -110,6 +144,12 @@ func EmptyInterfaceDemo(title string) {
 
 	//Use reflection to find an underlying type
 	fmt.Printf("\tY: %v, its type is: %v\n", y, reflect.ValueOf(y).Kind())
+
+	//Go 1.18+, use 'any' instead of interface{}
+	var anyVar any = "a string"
+	fmt.Printf("\tanyVar: %v, type: %[1]T\n", anyVar)
+	anyVar = 12
+	fmt.Printf("\tNOW: anyVar: %v, type: %[1]T\n", anyVar)
 
 	fmt.Printf("*** %v : END ***\n", title)
 	fmt.Printf("\n")
